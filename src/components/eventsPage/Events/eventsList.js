2ea27a503b6aabc2d95305cc3679811054f3e35e
Piotr Kwasniowski <kwasniowskipiotr5@gmail.com>
2024-06-17 13:06:09 +0200

    prepare almost all of main screen

---
+++ src/components/eventsPage/Events/eventsList.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function EventsList() {
+    const EventList = styled.div`
+
+    `
+    return (
+      <EventList>
+        
+      </EventList>  
+      
+    );
+  }
+  
+export default EventsList;
--- src/components/eventsPage/eventsPage.js
+++ src/components/eventsPage/eventsPage.js
@@ -1,6 +1,48 @@
+import styled from "styled-components";
+import EventsList from "./Events/eventsList";
+import { Link } from "react-router-dom";
+import './eventPage.css'
 function EventsPage() {
+    const Events = styled.div`
+      background-color: #525252;
+      align-self: center;
+      height: 80vh;
+      width: 50%;
+      text-align: center;
+    `
+    const Button = styled.button`
+    width: 50%;
+    height: 6vh;
+    background: #393C43;
+    box-shadow: 4px 8px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    font-family: 'Assistant';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 30px;
+    line-height: 63px;
+    margin-top: 5vh;
+    padding-bottom: 60px;
+    align-self: center;
+    color: #FFFFFF;
+    cursor:pointer;
+    user-select:none;
+    text-align: center;
+    text-decoration: none;
+    cursor: pointer;
+    transition-duration: 0.4s;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    `
     return (
-      <h1>Events</h1>
+      <div className="Body">
+        <Events>
+          <h1>Events History</h1>
+          <EventsList/>
+          <Button className="button no-border-but">
+                  <Link to="/create">Add new</Link>
+          </Button>
+        </Events> 
+      </div>
     );
   }
   
