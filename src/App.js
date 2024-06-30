8ea1ef1175716fddf057dad4971015d7fa56eba4
Piotr Kwasniowski <kwasniowskipiotr5@gmail.com>
2024-06-30 11:39:21 +0200

    working on auth cookies protecion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import EditGokarts from './components/GokartEditsPanel/editGokarts.js';
 import EventCreation from './components/eventCreation/eventCreation.js';
 import EventsPage from './components/eventsPage/eventsPage';
-import LoginPage from './components/loginPage/login.js';
+import LoginPage from './components/loginPage/Login.js';
 import EditPage from './components/mainEditPanel/editPage.js';
 import ViewPage from './components/mainViewPanel/viewPanel.js';
 import RaceStats from './components/raceStats/raceStats.js';
@@ -9,32 +9,38 @@ import RegisterPage from './components/registerPage/register';
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
 import FinishedStint from './components/stintEnd/stintEnd.js';
 import MainPage from './components/MainPage/MainPage.js';
+import AuthProvider from 'react-auth-kit';
+import AuthOutlet from '@auth-kit/react-router/AuthOutlet'
+import createStore from 'react-auth-kit/createStore';
 
+const router = createBrowserRouter([
+  {path: '/',
+  children: [
+    {path: '/', element: <MainPage/>},
+    {path: '/login', element: <LoginPage/>},
+    {path: '/Register', element: <RegisterPage/>},
+    {path: '/main', element:<AuthOutlet fallbackPath='/login' > <EventsPage/> </AuthOutlet>},
+    {path: '/edit', element:<AuthOutlet fallbackPath='/login' > <EditPage/> </AuthOutlet> },
+    {path: '/view', element:<AuthOutlet fallbackPath='/login' > <ViewPage/> </AuthOutlet> },
+    {path: '/create', element:<AuthOutlet fallbackPath='/login' > <EventCreation/> </AuthOutlet>},
+    {path: '/gokarts', element:<AuthOutlet fallbackPath='/login' > <EditGokarts/> </AuthOutlet>},
+    {path: '/stats', element:<AuthOutlet fallbackPath='/login' ><RaceStats/> </AuthOutlet> },
+    {path: '/stint', element:<AuthOutlet fallbackPath='/login' > <FinishedStint/> </AuthOutlet>},
+  ]},
+])
 
 function App() {
-  const router = createBrowserRouter([
-    {path: '/',
-    children: [
-      {path: '/', element: <MainPage/>},
-      {path: '/main', element: <EventsPage/>},
-      {path: '/edit', element: <EditPage/>},
-      {path: '/view', element: <ViewPage/>},
-      {path: '/create', element: <EventCreation/>},
-      {path: '/gokarts', element: <EditGokarts/>},
-      {path: '/stats', element: <RaceStats/>},
-      {path: '/stint', element: <FinishedStint/>},
-    ]},
-    {path: '/login',
-    children: [
-      {path: '/login', element: <LoginPage/>}
-    ]},
-    {path: '/Register',
-      children: [
-        {path: '/Register', element: <RegisterPage/>}
-    ]},
-  ])
+  const store = createStore({
+    authType: 'cookie', 
+    authName: '_auth', 
+    cookieDomain: window.location.hostname, 
+    cookieSecure: false 
+  });
   return (
-    <RouterProvider router={router} ></RouterProvider>
+    <AuthProvider store={store}>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    
   );
 }
 
